31cb694d45461bfa6ea4f5dbb63c52518742ca3f
mcf327 <136291869+mcf327@users.noreply.github.com>
2023-09-15 22:06:41 -0400

    Time: 154 ms (37.28%), Space: 51.4 MB (25.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const str = x.toString();
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+};

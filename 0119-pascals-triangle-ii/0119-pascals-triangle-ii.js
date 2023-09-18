03ef8b62c1148e79849e74e2128e24d8523198d0
mcf327 <136291869+mcf327@users.noreply.github.com>
2023-09-17 20:08:45 -0400

    Time: 46 ms (86.74%), Space: 42.3 MB (24.98%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    const result = new Array(rowIndex + 1).fill(0);
+
+    result[0] = 1;
+
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i; j >= 1; j--) {
+            result[j] += result[j - 1];
+        }
+    }
+
+    return result;
+};

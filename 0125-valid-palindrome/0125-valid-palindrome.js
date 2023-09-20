3862b3dd9c226061dd6fa9210b70d03470ab3df7
mcf327 <136291869+mcf327@users.noreply.github.com>
2023-09-20 13:27:18 -0400

    Time: 77 ms (17.53%), Space: 45.8 MB (59.2%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return str === str.split('').reverse().join('');
+};
